e422b694ae2cc5f80ed49e00d45abab2212440cc
TyHafen <tyhafen@gmail.com>
2022-05-15 11:38:43 -0600

    rock paper scissors

--- world.py
+++ world.py
@@ -1,38 +1,36 @@
 import random
-top_of_range = input("Type a number: ")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
-
-    if top_of_range <= 0:
-        print("please type a number larger than 0")
-        quit()
-else:
-    print("Type a number next time")
-    quit()
-
-
-r = random.randrange(top_of_range)
-guesses = 0
-
+user_wins = 0
+computer_wins = 0
+options = ["rock", "paper", "scissors"]
 
 while True:
-    guesses += 1
-    user_guess = input("make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-
-    else:
-        print("Type a number next time")
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+    if user_input not in options:
         continue
 
-    if user_guess == r:
-        print("You got it!")
-        break
-    else:
-        if user_guess > r:
-            print("You were above the number!")
-        else:
-            print("You were below the number!")
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked:", computer_pick + ".")
 
-print("you got it in", guesses, "guesses")
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You Won!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You Won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You Won!")
+        user_wins += 1
+
+    else:
+        print("You Lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
