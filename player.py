20b4372279e2c58c17bd5f2312e60780f0baed83
TyHafen <tyhafen@gmail.com>
2022-05-17 11:35:53 -0600

    tic tac toe

--- player.py
+++ player.py
@@ -8,3 +8,19 @@ class Player:
 
     def get_move(self, game):
         pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
